c0e06d6c804e0c4e96c60a803ef3088216bdf2e9
holzru <holzru@gmail.com>
2016-09-29 15:01:48 -0700

    closer and closer

--- prac3.js
+++ prac3.js
@@ -24,3 +24,41 @@ function reverseVowels(str) {
 
 console.log(reverseVowels('hello'));
 console.log(reverseVowels('emy'));
+
+function maxTreePath(rrot) {
+  let pathStack = [],
+      valueStack = [],
+      path = [],
+      cpv = root.value,
+      maxPath = [],
+      max = root.value;
+  function explorer(node) {
+    if (!node) {return;}
+    if (node !== root) {
+      if (node.value > CPV && CPV < 0) {
+        pathStack.push(path);
+        valueStack.push(cpv);
+        path = [node];
+        cpv = node.value;
+      } else {
+        path.push(node);
+        cpv += node.value;
+      }
+      if (cpv > max) {
+        max = cpv;
+        maxPath = path;
+      }
+      for (let i = 0; i < node.children.length; i++) {
+        explorer(node.children[i])
+      }
+      path.pop();
+      cpv -= node.value;
+      if (!path.length) {
+        path = pathStack.pop();
+        cpv = valueStack.pop();
+      }
+    }
+  }
+  explorer(node);
+  return maxPath;
+}
